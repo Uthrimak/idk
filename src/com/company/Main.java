23ff6d696dfcefb7be2e8422c15164f88f0c5262
Thomas Diggs <33760531+Uthrimak@users.noreply.github.com>
2020-03-04 19:05:39 -0600

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello, world!");
+	    String staticVariable = "dog";
+	    char charVariable = 'd';
+	    System.out.println(staticVariable.charAt(1));
+	    int intVar = 42;
+    }
+}
